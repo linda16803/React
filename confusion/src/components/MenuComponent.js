d2c2a7b1378f4db8ef42ca9dbfd4b1a057b34b9e
linda huang <huanglihong2008@gmail.com>
2020-02-25 23:31:19 -0500

    Commit update menu codes.

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -49,23 +49,33 @@ class Menu extends Component {
     }
 
     render() {
-        const menu = this.state.dishes.map((dish)=> {
+        const menu = this.state.dishes.map((dish) => {
             return (
-                <div key ={dish.id} className="col-12 mt-5">
-                    
-                </div>
-            )
+              <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
         });
+
         return (
-            <div className="container">
-                <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
-                </div>
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
             </div>
+          </div>
         );
     }
 }
+
 
 export default Menu;
