cc2e7c6c90853388f2d41545921dd2b5af04632a
linda huang <huanglihong2008@gmail.com>
2020-02-26 22:31:53 -0500

    execise number 2 to submit

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React , {Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MenuComponent';
 
-function App() {
+import { DISHES } from './shared/dishes';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -12,9 +22,11 @@ function App() {
           <NavbarBrand href="/">Ristonrate Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
 }
 
+
+}
 export default App;
